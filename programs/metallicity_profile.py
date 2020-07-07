d5c50cf557eaff469d3219f8273503b849f987aa
ornemarioni <ornelamarioni@gmail.com>
2020-07-07 02:07:00 +0200

    perfiles radiales de metalicidad y z50

---
+++ programs/metallicity_profile.py
@@ -0,0 +1,19 @@
+import numpy as np
+import bines2
+
+#----------------------------------------------------------------------
+# Half-mass scale height
+#----------------------------------------------------------------------
+def met(R,z,FeH,nbin):
+    
+    med, nodos = bines2.rbin1(R, nbin)
+    
+    Fe_H = np.zeros(nbin)
+    
+    for i in range(nbin):
+        
+        mask, = np.where((R < nodos[i+1]) & (R > nodos[i]) & (z>0))
+        
+        Fe_H[i] = np.median(FeH[mask])
+        
+    return med, Fe_H
---
+++ programs/z50.py
@@ -0,0 +1,27 @@
+import numpy as np
+import bines2
+
+#----------------------------------------------------------------------
+# Half-mass scale height
+#----------------------------------------------------------------------
+def HMSH(R,z,m,nbin):
+    
+    med, nodos = bines2.rbin1(R, nbin)
+    
+    z50 = np.zeros(nbin)
+    
+    for i in range(nbin):
+        
+        mask, = np.where((R < nodos[i+1]) & (R > nodos[i]) & (z>0))
+        
+        zorder = np.argsort(z[mask])
+        
+        zsort = z[mask][zorder]
+        msort = m[mask][zorder]
+        
+        mtot   = np.cumsum(msort)
+        m_mean, = np.where(mtot < mtot[-1]/2.)
+        
+        z50[i] = zsort[m_mean][-1]
+        
+    return med, z50
