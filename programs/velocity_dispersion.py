4701a43acb09ae0208ca2a184f336019fc22d79d
ornemarioni <ornelamarioni@gmail.com>
2022-09-30 19:32:10 +0200

    vel disp for ages

--- programs/velocity_dispersion.py
+++ programs/velocity_dispersion.py
@@ -1,15 +1,15 @@
-import numpy
+import numpy as np
 
 #-------------------------------------------------------------------------------------------------
 # VELOCITY DISPERTION 1-D
 #-------------------------------------------------------------------------------------------------
 
-def vel_disp_prof(v):
+def vel_disp(v):
     
     v_mean = np.mean(v)
 
     v_sqdiff = (v - v_mean)**2
-    v_disp2 = np.sum(v_sqdiff)/(len(vz)-1)
+    v_disp2 = np.sum(v_sqdiff/(len(v)-1))
     v_disp = np.sqrt(v_disp2)
         
     return v_disp
